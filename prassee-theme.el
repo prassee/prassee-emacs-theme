e4aaa2fcbf26e2542eb81bca73d712e058ad5509
prassee <prassee.sathian@gmail.com>
2017-02-20 20:51:53 +0530

    updated package header

--- prassee-theme.el
+++ prassee-theme.el
@@ -1,12 +1,25 @@
+;;; prassee-theme.el --- Dark color theme
+
+;; Copyright (c) 2017 Prassee
+
+;; Author: Prassee  <prassee.sathian@gmail.com>
+;; Keywords: theme dark colors
+;; URL: http://github.com/fgeller/basic-theme.el
+;; Package-Requires: ((emacs "24"))
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
 ;; Inspired from Doom-theme with some customization
-;;; prassee-theme.el --- A dark contrast color theme for Emacs.
-
-;; Copyright (C) 2011-2016 Prassee
-
-;; Author: Prassee <prassee.sathian@gmail.com>
-;; URL: http://github.com/prassee/prassee-emacs-theme
-;; Version: 1.0
-
 
 (deftheme prassee
   "A dark theme inspired by Doom One theme")
@@ -369,3 +382,8 @@
 
 (provide-theme 'prassee)
 
+;; Local Variables:
+;; no-byte-compile: t
+;; indent-tabs-mode: nil
+;; End:
+;;; prassee-theme.el ends here
