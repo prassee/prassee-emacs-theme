7ce96272a514caa8598aa3c7227ade8a21e20daa
prassee <prassee.sathian@gmail.com>
2017-04-07 08:23:46 +0530

    update to version 1.2

--- prassee-theme.el
+++ prassee-theme.el
@@ -6,7 +6,7 @@
 ;; Keywords: faces
 ;; URL: https://github.com/prassee/prassee-emacs-theme
 ;; Package-Requires: ((emacs "24"))
-;; Version: 1.1
+;; Version: 1.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
