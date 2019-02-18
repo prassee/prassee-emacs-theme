03e1ffc7932eca33e259aefaddb7a1d3ea4fbe4e
nakasu <nakashima.f.s.h@gmail.com>
2019-02-18 10:17:20 +0900

    Fix autoload comment

--- prassee-theme.el
+++ prassee-theme.el
@@ -370,7 +370,7 @@
 ;; set cursor style
 (setq-default cursor-type '(bar . 5))
 
-;; autoload
+;;;###autoload
 (when load-file-name
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
